7989c8617e8f0ea9e6077f0d4241ce158365e12e
Lennart Schedin <lennart.schedin@gmail.com>
2023-11-24 13:34:06 +0100

    minor reformat

--- src/test/java/org/apache/maven/plugins/jarsigner/MyProjectStub.java
+++ src/test/java/org/apache/maven/plugins/jarsigner/MyProjectStub.java
@@ -32,53 +32,47 @@ import org.codehaus.plexus.util.ReaderFactory;
 import org.junit.Test;
 
 public class MyProjectStub
-extends MavenProjectStub
-{
-/**
- * Default constructor
- */
-public MyProjectStub()
-{
-    MavenXpp3Reader pomReader = new MavenXpp3Reader();
-    Model model;
-    try
-    {
-        model = pomReader.read( ReaderFactory.newXmlReader( new File( getBasedir(), "pom.xml" ) ) );
-        setModel( model );
+    extends MavenProjectStub {
+    /**
+     * Default constructor
+     */
+    public MyProjectStub() {
+        MavenXpp3Reader pomReader = new MavenXpp3Reader();
+        Model model;
+        try {
+            model = pomReader.read(ReaderFactory.newXmlReader(new File(getBasedir(), "pom.xml")));
+            setModel(model);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        setGroupId(model.getGroupId());
+        setArtifactId(model.getArtifactId());
+        setVersion(model.getVersion());
+        setName(model.getName());
+        setUrl(model.getUrl());
+        setPackaging(model.getPackaging());
+
+        Build build = new Build();
+        build.setFinalName(model.getArtifactId());
+        build.setDirectory(getBasedir() + "/target");
+        build.setSourceDirectory(getBasedir() + "/src/main/java");
+        build.setOutputDirectory(getBasedir() + "/target/classes");
+        build.setTestSourceDirectory(getBasedir() + "/src/test/java");
+        build.setTestOutputDirectory(getBasedir() + "/target/test-classes");
+        setBuild(build);
+
+        List compileSourceRoots = new ArrayList();
+        compileSourceRoots.add(getBasedir() + "/src/main/java");
+        setCompileSourceRoots(compileSourceRoots);
+
+        List testCompileSourceRoots = new ArrayList();
+        testCompileSourceRoots.add(getBasedir() + "/src/test/java");
+        setTestCompileSourceRoots(testCompileSourceRoots);
     }
-    catch ( Exception e )
-    {
-        throw new RuntimeException( e );
+
+    /** {@inheritDoc} */
+    public File getBasedir() {
+        return new File(super.getBasedir() + "/src/test/resources/unit/project-to-test/");
     }
-
-    setGroupId( model.getGroupId() );
-    setArtifactId( model.getArtifactId() );
-    setVersion( model.getVersion() );
-    setName( model.getName() );
-    setUrl( model.getUrl() );
-    setPackaging( model.getPackaging() );
-
-    Build build = new Build();
-    build.setFinalName( model.getArtifactId() );
-    build.setDirectory( getBasedir() + "/target" );
-    build.setSourceDirectory( getBasedir() + "/src/main/java" );
-    build.setOutputDirectory( getBasedir() + "/target/classes" );
-    build.setTestSourceDirectory( getBasedir() + "/src/test/java" );
-    build.setTestOutputDirectory( getBasedir() + "/target/test-classes" );
-    setBuild( build );
-
-    List compileSourceRoots = new ArrayList();
-    compileSourceRoots.add( getBasedir() + "/src/main/java" );
-    setCompileSourceRoots( compileSourceRoots );
-
-    List testCompileSourceRoots = new ArrayList();
-    testCompileSourceRoots.add( getBasedir() + "/src/test/java" );
-    setTestCompileSourceRoots( testCompileSourceRoots );
-}
-
-/** {@inheritDoc} */
-public File getBasedir()
-{
-    return new File( super.getBasedir() + "/src/test/resources/unit/project-to-test/" );
-}
 }
